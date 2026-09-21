655adc7c3078a6ab59fe356ee3b641e23a4ff72f
KinneyB42 <34866101+KinneyB42@users.noreply.github.com>
2026-09-21 02:12:09 -0500

    Fix JMGO affiliate links: drop www subdomain (no DNS record), use Impact-generated non-www vanity links for IRIS ULTRA and IRIS ULTRA MAX; bump main.js cache to v20260921d

--- js/main.js
+++ js/main.js
@@ -34,8 +34,8 @@
     amazonSearch: 'https://www.amazon.com/s?k=',
     ebayCampid: '5339116943', /* Brandon's EPN campaign, confirmed 2026-09-20 */
     ebayMkrid: '711-53200-19255-0',
-    jmgoIrisUltra: 'https://www.affiliate.jmgo.com/OYv0dG', /* Brandon's JMGO affiliate link, supplied 2026-09-21 */
-    jmgoIrisUltraMax: 'https://www.affiliate.jmgo.com/JkvXAN', /* Brandon's JMGO affiliate link, supplied 2026-09-21 */
+    jmgoIrisUltra: 'https://affiliate.jmgo.com/OYv0dG', /* Brandon's JMGO affiliate link, generated via Impact 2026-09-21 */
+    jmgoIrisUltraMax: 'https://affiliate.jmgo.com/JkvXAN', /* Brandon's JMGO affiliate link, generated via Impact 2026-09-21 */
   };
   function isJmgoFeatured() {
     return selectedModel && selectedModel.b === 'JMGO' &&
