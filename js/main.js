c5dbe675faf04c9c3c5d2a0ca1e458394492c11c
KinneyB42 <34866101+KinneyB42@users.noreply.github.com>
2026-09-21 01:54:34 -0500

    Calculator: JMGO IRIS ULTRA MAX direct affiliate link

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@
     ebayCampid: '5339116943', /* Brandon's EPN campaign, confirmed 2026-09-20 */
     ebayMkrid: '711-53200-19255-0',
     jmgoIrisUltra: '', /* Brandon to supply: JMGO direct affiliate URL for IRIS ULTRA */
-    jmgoIrisUltraMax: '' /* Brandon to supply: JMGO direct affiliate URL for IRIS ULTRA MAX */
+    jmgoIrisUltraMax: 'https://www.affiliate.jmgo.com/JkvXAN', /* Brandon's JMGO affiliate link, supplied 2026-09-21 */
   };
   function isJmgoFeatured() {
     return selectedModel && selectedModel.b === 'JMGO' &&
